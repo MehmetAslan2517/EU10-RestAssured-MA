0e2c14e829e3562d2f670cf2d0a1f65fa202c6c8
Mehmet Aslan <mehmetaslan2001@gmail.com>
2023-04-05 13:48:27 +0300

    Completely commit

--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -1,5 +1,6 @@
 package com.cydeo.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -8,7 +9,24 @@ public class SpartanTestBase {
 
     @BeforeAll
     public static void init(){
-        baseURI = "http://54.144.125.207:8000";
+        baseURI = ConfigurationReader.getProperty("spartanBaseURL");
+
+        //we need to connect DB, so we use DBUtils.createConnection() method
+        //later we can use ConfigurationReader for dbUrl,dbUsername,dbPassword
+       // String dbUrl = "jdbc:oracle:thin:@54.144.125.207:1521:XE";
+       // String dbUsername = "SP";
+       // String dbPassword = "SP";
+
+       // DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+
+        DBUtils.createConnection(ConfigurationReader.getProperty("spartanDbUrl"),
+                ConfigurationReader.getProperty("spartanDbUsername"),
+                ConfigurationReader.getProperty("spartanDbPassword"));
+    }
+
+    @AfterAll
+    public static void teardown(){
+        DBUtils.destroy();
     }
 
 }
